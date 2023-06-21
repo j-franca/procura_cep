0f1df02aff1333300c8375d693df74ce6c2443e2
Júlia <jfranca.contato@gmail.com>
2023-06-21 12:13:39 -0300

    21

--- prog_cep.js
+++ prog_cep.js
@@ -13,10 +13,17 @@ consulta.addEventListener('click', ()=>{
     console.log(cep)
 
     cep.onload = function(){
+        // document.getElementById('cep_cidade').innerHTML
         let pesq_cep = JSON.parse(cep.responseText)
         console.log(pesq_cep)
 
         res.innerHTML = ''
-        res.innerHTML = `O CEP refere ${pesq_cep.localidade} na rua ${pesq_cep.logradoura} no bairro ${pesq_cep.bairro}`
+
+        if((pesq_cep.bairro === 'undefined')){
+            res.innerHTML = `O CEP refere ${pesq_cep.localidade}`
+        }else{
+            res.innerHTML = `O CEP refere ${pesq_cep.localidade} na rua ${pesq_cep.logradouro} no bairro ${pesq_cep.bairro}`
+        }
+        res.innerHTML = `O CEP refere ${pesq_cep.localidade} na rua ${pesq_cep.logradouro} no bairro ${pesq_cep.bairro}`
     }
 })
