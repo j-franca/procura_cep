6aa096ce1b3255d9486ba1b15338bcdc5feff3c9
Júlia <jfranca.contato@gmail.com>
2023-06-28 08:11:48 -0300

    formulario

--- prog_cep.js
+++ prog_cep.js
@@ -1,7 +1,9 @@
 let consulta = document.getElementById('consulta')
+let estado = document.getElementById('estado')
+let cidade = document.getElementById('cidade')
+let bairro = document.getElementById('bairro')
+let rua = document.getElementById('rua')
 let res = document.getElementById('res')
-
-console.log(consulta)
 
 consulta.addEventListener('click', ()=>{
     let valor_cep = document.getElementById('valor_cep').value
@@ -15,15 +17,27 @@ consulta.addEventListener('click', ()=>{
     cep.onload = function(){
         // document.getElementById('cep_cidade').innerHTML
         let pesq_cep = JSON.parse(cep.responseText)
+        
+        pesq_cep.uf = estado.innerHTML
+        pesq_cep.localidade = cidade.innerHTML
+        pesq_cep.bairro = bairro.innerHTML
+        pesq_cep.logradouro = rua.innerHTML
+        
         console.log(pesq_cep)
+        
+        // res.innerHTML = ''
 
-        res.innerHTML = ''
-
-        if((pesq_cep.bairro === 'undefined')){
-            res.innerHTML = `O CEP refere ${pesq_cep.localidade}`
-        }else{
-            res.innerHTML = `O CEP refere ${pesq_cep.localidade} na rua ${pesq_cep.logradouro} no bairro ${pesq_cep.bairro}`
-        }
-        res.innerHTML = `O CEP refere ${pesq_cep.localidade} na rua ${pesq_cep.logradouro} no bairro ${pesq_cep.bairro}`
+        // if((pesq_cep.bairro === 'undefined')){
+        //     res.innerHTML = `O CEP refere ${pesq_cep.localidade}`
+        // }else{
+        //     res.innerHTML = `O CEP refere ${pesq_cep.localidade} na rua ${pesq_cep.logradouro} no bairro ${pesq_cep.bairro}`
+        // }
+        // res.innerHTML = `O CEP refere ${pesq_cep.localidade} na rua ${pesq_cep.logradouro} no bairro ${pesq_cep.bairro}`
     }
+})
+
+// cadastrado
+const cadastrado = document.querySelector('#cadastrado')
+cadastrado.addEventListener('click', ()=>{
+    alert('Cadastrado com sucesso!')
 })
